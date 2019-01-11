a92a864aae7165409ccaa6cc62018a7353b1a452
Brian Faust <hello@basecode.sh>
2019-01-11 08:52:28 +0200

    test: sortBy

---
+++ __tests__/sortBy.test.ts
@@ -0,0 +1,20 @@
+import { sortBy } from "../src";
+
+const dummies = [
+    { name: "John", age: 30 },
+    { name: "Jane", age: 40 },
+    { name: "Andrew", age: 18 },
+    { name: "Bob", age: 18 },
+];
+
+test("sorts records by string", () => {
+    const actual = sortBy(dummies, "age");
+
+    expect(actual).toEqual(dummies);
+});
+
+test("sorts records by array", () => {
+    const actual = sortBy(dummies, ["age"]);
+
+    expect(actual).toEqual(dummies);
+});
