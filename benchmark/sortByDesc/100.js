87759ea3669421902656bbb1b96f6e539a84289c
Brian Faust <hello@basecode.sh>
2019-01-16 13:55:45 +0200

    test: remove some meaningless native benchmarks

--- benchmark/sortByDesc/100.js
+++ benchmark/sortByDesc/100.js
@@ -1,14 +1,9 @@
 const {
-    native,
     utils,
     lodash
 } = require('./methods')
 
 const items = require('../helpers').createWallets(100);
-
-exports['native'] = () => {
-    return native(items);
-};
 
 exports['utils'] = () => {
     return utils(items);
--- benchmark/sortByDesc/methods.js
+++ benchmark/sortByDesc/methods.js
@@ -1,6 +1,5 @@
 const utils = require('../../dist')
 const lodash = require('lodash/orderBy')
 
-exports.native = items => items.sort((a, b) => b.producedBlocks - a.producedBlocks)
 exports.utils = items => utils.sortByDesc(items, 'producedBlocks')
 exports.lodash = items => lodash(items, ['producedBlocks'], ['desc'])
