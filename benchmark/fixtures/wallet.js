64d2c4fbe32d8d9f4ae99124e38dc04d9cf880f3
Brian Faust <hello@basecode.sh>
2019-01-16 09:25:16 +0200

    test: handle benchmark fixtures as classes like in core

---
+++ benchmark/fixtures/wallet.js
@@ -0,0 +1,21 @@
+class Wallet {
+    constructor() {
+        this.address = "DNjuJEDQkhrJ7cA9FZ2iVXt5anYiM8Jtc9"
+        this.balance = "43964655884383"
+        this.dirty = false
+        this.forgedFees = "109329149476"
+        this.forgedRewards = "4835000000000"
+        this.missedBlocks = 142
+        this.multisignature = null
+        this.producedBlocks = 24389
+        this.publicKey = "03bbfb43ecb5a54a1e227bb37b5812b5321213838d376e2b455b6af78442621dec"
+        this.rate = 2
+        this.secondPublicKey = null
+        this.username = "arkx"
+        this.vote = "03bbfb43ecb5a54a1e227bb37b5812b5321213838d376e2b455b6af78442621dec"
+        this.voteBalance = "45580764424106"
+        this.voted = true
+    }
+}
+
+module.exports = new Wallet()
