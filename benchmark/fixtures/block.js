415edd82621dd136b18a0b361efda776d2447136
Brian Faust <hello@basecode.sh>
2019-01-17 09:48:12 +0200

    test: use real crypto models for benchmarks

--- benchmark/fixtures/block.js
+++ benchmark/fixtures/block.js
@@ -1,22 +1,21 @@
-class Block {
-    constructor() {
-        this.blockSignature = "3045022100ec0ab2d915ff93f7af49a52c463d915758ca06d447cf9adab4cec1131065971702203c5702fbebc185f369c43219c15175ed4866bf0d4308e41598c58851246a597d"
-        this.generatorPublicKey = "036a520acf24036ff691a4f8ba19514828e9b5aa36ca4ba0452e9012023caccfef"
-        this.height = 1201204
-        this.id = "2653480465582386466"
-        this.idHex = "24d30e6cb979ed22"
-        this.numberOfTransactions = 0
-        this.payloadHash = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-        this.payloadLength = 0
-        this.previousBlock = "14512262220230802923"
-        this.previousBlockHex = "c965e9999c2dbdeb"
-        this.reward = 200000000
-        this.timestamp = 57337218
-        this.totalAmount = 0
-        this.totalFee = 0
-        this.transactions = []
-        this.version = 0
-    }
-}
+const { models: { Block } } = require("@arkecosystem/crypto");
 
-module.exports = new Block()
+const block = new Block();
+block.blockSignature = "3045022100ec0ab2d915ff93f7af49a52c463d915758ca06d447cf9adab4cec1131065971702203c5702fbebc185f369c43219c15175ed4866bf0d4308e41598c58851246a597d"
+block.generatorPublicKey = "036a520acf24036ff691a4f8ba19514828e9b5aa36ca4ba0452e9012023caccfef"
+block.height = 1201204
+block.id = "2653480465582386466"
+block.idHex = "24d30e6cb979ed22"
+block.numberOfTransactions = 0
+block.payloadHash = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+block.payloadLength = 0
+block.previousBlock = "14512262220230802923"
+block.previousBlockHex = "c965e9999c2dbdeb"
+block.reward = 200000000
+block.timestamp = 57337218
+block.totalAmount = 0
+block.totalFee = 0
+block.transactions = []
+block.version = 0
+
+module.exports = block
--- benchmark/fixtures/wallet.js
+++ benchmark/fixtures/wallet.js
@@ -1,21 +1,20 @@
-class Wallet {
-    constructor() {
-        this.address = "DNjuJEDQkhrJ7cA9FZ2iVXt5anYiM8Jtc9"
-        this.balance = "43964655884383"
-        this.dirty = false
-        this.forgedFees = "109329149476"
-        this.forgedRewards = "4835000000000"
-        this.missedBlocks = 142
-        this.multisignature = null
-        this.producedBlocks = 24389
-        this.publicKey = "03bbfb43ecb5a54a1e227bb37b5812b5321213838d376e2b455b6af78442621dec"
-        this.rate = 2
-        this.secondPublicKey = null
-        this.username = "arkx"
-        this.vote = "03bbfb43ecb5a54a1e227bb37b5812b5321213838d376e2b455b6af78442621dec"
-        this.voteBalance = "45580764424106"
-        this.voted = true
-    }
-}
+const { models: { Wallet } } = require("@arkecosystem/crypto");
 
-module.exports = new Wallet()
+const wallet = new Wallet();
+wallet.address = "DNjuJEDQkhrJ7cA9FZ2iVXt5anYiM8Jtc9"
+wallet.balance = "43964655884383"
+wallet.dirty = false
+wallet.forgedFees = "109329149476"
+wallet.forgedRewards = "4835000000000"
+wallet.missedBlocks = 142
+wallet.multisignature = null
+wallet.producedBlocks = 24389
+wallet.publicKey = "03bbfb43ecb5a54a1e227bb37b5812b5321213838d376e2b455b6af78442621dec"
+wallet.rate = 2
+wallet.secondPublicKey = null
+wallet.username = "arkx"
+wallet.vote = "03bbfb43ecb5a54a1e227bb37b5812b5321213838d376e2b455b6af78442621dec"
+wallet.voteBalance = "45580764424106"
+wallet.voted = true
+
+module.exports = wallet
