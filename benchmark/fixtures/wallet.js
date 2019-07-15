a650ab74b6c16d86b6cbebb9f5f435e2374e9ea9
Brian Faust <hello@basecode.sh>
2019-07-15 08:26:13 +0300

    chore(deps): update all dependencies to their latest versions

--- benchmark/fixtures/wallet.js
+++ benchmark/fixtures/wallet.js
@@ -1,20 +1,20 @@
-const { models: { Wallet } } = require("@arkecosystem/crypto");
+class Wallet {}
 
 const wallet = new Wallet();
-wallet.address = "DNjuJEDQkhrJ7cA9FZ2iVXt5anYiM8Jtc9"
-wallet.balance = "43964655884383"
-wallet.dirty = false
-wallet.forgedFees = "109329149476"
-wallet.forgedRewards = "4835000000000"
-wallet.missedBlocks = 142
-wallet.multisignature = null
-wallet.producedBlocks = 24389
-wallet.publicKey = "03bbfb43ecb5a54a1e227bb37b5812b5321213838d376e2b455b6af78442621dec"
-wallet.rate = 2
-wallet.secondPublicKey = null
-wallet.username = "arkx"
-wallet.vote = "03bbfb43ecb5a54a1e227bb37b5812b5321213838d376e2b455b6af78442621dec"
-wallet.voteBalance = "45580764424106"
-wallet.voted = true
+wallet.address = "DNjuJEDQkhrJ7cA9FZ2iVXt5anYiM8Jtc9";
+wallet.balance = "43964655884383";
+wallet.dirty = false;
+wallet.forgedFees = "109329149476";
+wallet.forgedRewards = "4835000000000";
+wallet.missedBlocks = 142;
+wallet.multisignature = null;
+wallet.producedBlocks = 24389;
+wallet.publicKey = "03bbfb43ecb5a54a1e227bb37b5812b5321213838d376e2b455b6af78442621dec";
+wallet.rate = 2;
+wallet.secondPublicKey = null;
+wallet.username = "arkx";
+wallet.vote = "03bbfb43ecb5a54a1e227bb37b5812b5321213838d376e2b455b6af78442621dec";
+wallet.voteBalance = "45580764424106";
+wallet.voted = true;
 
-module.exports = wallet
+module.exports = wallet;
