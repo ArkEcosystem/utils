5b4708dab4a49e39bd075ec8dad85283cd503990
Brian Faust <hello@basecode.sh>
2019-09-19 18:13:14 +0300

    refactor: improve get performance by 3x

--- __tests__/get.test.ts
+++ __tests__/get.test.ts
@@ -3,11 +3,11 @@ import "jest-extended";
 import { get } from "../src";
 
 describe("#get", () => {
-    it("should work like lodash", () => {
-        const object = { a: [{ b: { c: 3 } }] };
+    it("should work with nested objects", () => {
+        const object = { a: { b: { c: 3 } } };
 
-        expect(get(object, "a[0].b.c")).toEqual(3);
-        expect(get(object, ["a", "0", "b", "c"])).toEqual(3);
-        expect(get(object, "a.b.c", "default")).toEqual("default");
+        expect(get(object, "a.b.c")).toEqual(3);
+        expect(get(object, ["a", "b", "c"])).toEqual(3);
+        expect(get(object, "a.b.c.d", "default")).toEqual("default");
     });
 });
--- src/get.ts
+++ src/get.ts
@@ -1,5 +1,24 @@
 import { get as _get } from "dot-prop";
-import { transformBrackets } from "./internal";
+import { isString } from "./is-string";
 
-export const get = <T>(object: object, path: string | string[], defaultValue?: T): T | undefined =>
-    _get(object, transformBrackets(path), defaultValue);
+/**
+ * This behaves like lodash.get with the major difference of
+ * not supporting square brackets which nets a 3x performance gain.
+ */
+export const get = <T>(object: object, path: string | string[], defaultValue?: T): T | undefined => {
+    const fragments: string[] = isString(path) ? path.split(".") : path;
+
+    let index = 0;
+    const length: number = fragments.length;
+
+    while (object != null && index < length) {
+        object = object[fragments[index++]];
+    }
+
+    if (index && index === length) {
+        // @ts-ignore
+        return object || defaultValue;
+    }
+
+    return defaultValue;
+};
