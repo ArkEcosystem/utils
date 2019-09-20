40d9b3a2a319c04ca353130f2fccde567e7a54a2
Brian Faust <hello@basecode.sh>
2019-09-20 10:12:26 +0300

    perf: return early in isEqual if the values don't need to be stringified

--- src/is-equal.ts
+++ src/is-equal.ts
@@ -1,3 +1,13 @@
 import { stringify } from "./stringify";
 
-export const isEqual = (actual: unknown, expected: unknown): boolean => stringify(actual) === stringify(expected);
+export const isEqual = (actual: unknown, expected: unknown): boolean => {
+    if (actual === expected) {
+        return true;
+    }
+
+    if (actual == null || expected == null) {
+        return actual !== actual && expected !== expected;
+    }
+
+    return stringify(actual) === stringify(expected);
+};
