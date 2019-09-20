0409beee9fb96cc898217f2964c684b569e44e68
Brian Faust <hello@basecode.sh>
2019-09-20 18:43:13 +0300

    perf: improve removeCase performance by 20%

--- src/internal/remove-case/index.ts
+++ src/internal/remove-case/index.ts
@@ -1,19 +1,22 @@
 import { toLower } from "../../to-lower";
 import { CAMEL_CASE_REGEXP } from "./camel-case-regexp";
 import { CAMEL_CASE_UPPER_REGEXP } from "./camel-case-upper-regexp";
-import { NON_WORD_REGEXP } from "./non-word-regexp";
 
 // Based on https://github.com/blakeembrey/no-case
-export const removeCase = (str: string): string =>
+export const removeCase = (input: string): string =>
     toLower(
-        str
+        input
             // Support camel case ("camelCase" -> "camel Case").
             .replace(CAMEL_CASE_REGEXP, "$1 $2")
             // Support odd camel case ("CAMELCase" -> "CAMEL Case").
             .replace(CAMEL_CASE_UPPER_REGEXP, "$1 $2")
-            // Remove all non-word characters and replace with a single space.
-            .replace(NON_WORD_REGEXP, (match, index, value) =>
-                /* istanbul ignore next */
-                index === 0 || index === value.length - match.length ? "" : " ",
-            ),
+            // Match words composed of alphanumeric characters.
+            .match(/[^\x00-\x2f\x3a-\x40\x5b-\x60\x7b-\x7f]+/g)
+            .join(" "),
     );
+
+// This is 300% faster but unable to handle camelCase.
+// input
+//     .match(/[^\x00-\x2f\x3a-\x40\x5b-\x60\x7b-\x7f]+/g)
+//     .join(" ")
+//     .toLowerCase();
