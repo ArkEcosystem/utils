0ce33453ca3f7b0acf6ee2c46731a9fa2e54ae2f
Brian Faust <hello@basecode.sh>
2019-09-23 06:14:57 +0300

    perf: improve min and max performance by 7x

--- src/min.ts
+++ src/min.ts
@@ -1,1 +1,11 @@
-export const min = (values: number[]): number => Math.min(...values);
+export const min = (values: number[]): number => {
+    let min: number = values[0];
+
+    for (let i = 0; i < values.length; i++) {
+        const value: number = values[i];
+
+        min = value < min ? value : min;
+    }
+
+    return min;
+};
