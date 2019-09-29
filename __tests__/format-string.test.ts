a7daa80106efb8eca4ad71381ea4097001f12d56
Brian Faust <faustbrian@users.noreply.github.com>
2019-09-29 06:39:58 +0300

    feat: implement formatBytes and formatNumber methods (#28)

--- __tests__/format-string.test.ts
+++ __tests__/format-string.test.ts
@@ -3,7 +3,7 @@ import "jest-extended";
 import { formatString } from "../src";
 
 describe("#formatString", () => {
-    it("should formatString the string with an explicit positional index", () => {
+    it("should format the string with an explicit positional index", () => {
         expect(formatString("{0} World", "Hello")).toBe("Hello World");
     });
 });
