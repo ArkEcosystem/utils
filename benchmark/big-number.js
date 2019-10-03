aa4bca9529a0f7dfe42fc049b9efe6050c37eec1
Brian Faust <hello@basecode.sh>
2019-10-03 05:03:53 +0300

    chore: add more BigNumber benchmarks

--- benchmark/big-number.js
+++ benchmark/big-number.js
@@ -1,10 +1,16 @@
 const {
     BigNumber
 } = require('../dist')
-const BigNum = require('bignumber.js')
+const BigNumberJS = require('bignumber.js')
+const BigJS = require('bignumber.js')
+const BigNumber2 = require('big-number')
 
 exports['native'] = () => BigInt('1111222233334444555566')
 
 exports['utils'] = () => new BigNumber('1111222233334444555566')
 
-exports['bignumber.js'] = () => new BigNum('1111222233334444555566')
+exports['bignumber.js'] = () => new BigNumberJS('1111222233334444555566')
+
+exports['big.js'] = () => new BigJS('1111222233334444555566')
+
+exports['big-number'] = () => new BigNumber2('1111222233334444555566')
