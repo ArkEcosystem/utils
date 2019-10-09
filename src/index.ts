6df884c0f288afe0211c5a8b5aeab766e026ce83
Brian Faust <hello@basecode.sh>
2019-10-09 05:39:12 +0300

    feat: implement expandTilde method

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,7 @@ export * from "./concat";
 export * from "./constant-case";
 export * from "./dot-case";
 export * from "./every";
+export * from "./expand-tilde";
 export * from "./fill";
 export * from "./filter-array";
 export * from "./filter-object";
