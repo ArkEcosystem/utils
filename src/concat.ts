f122ab0f134dad640765d7b425c8717348298aff
Brian Faust <hello@basecode.sh>
2019-10-10 13:53:51 +0300

    fix: spread any arguments for concat

--- src/concat.ts
+++ src/concat.ts
@@ -1,10 +1,10 @@
 import { isArray } from "./is-array";
 
-export const concat = <T>(...values: T[]): T[] => {
+export const concat = <T>(...values: any[]): T[] => {
     const result: T[] = [];
 
     for (let i = 0; i < values.length; i++) {
-        const item: T = values[i];
+        const item: T | T[] = values[i];
 
         if (isArray(item)) {
             const childLength: number = item.length;
