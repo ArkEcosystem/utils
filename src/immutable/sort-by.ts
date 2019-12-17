e079b781b686f87f46a0fc17d9bc216a166ebeb1
Brian Faust <hello@basecode.sh>
2019-12-17 07:11:45 +0200

    fix: types

--- src/immutable/sort-by.ts
+++ src/immutable/sort-by.ts
@@ -1,4 +1,8 @@
-import { Iteratee } from "../internal";
+import { ISortBy, ISortByFunction } from "fast-sort";
+
 import { sortBy as baseSortBy } from "../sort-by";
 
-export const sortBy = <T>(values: T[], iteratees?: Iteratee | Iteratee[]): T[] => baseSortBy([...values], iteratees);
+export const sortBy = <T>(
+    values: T[],
+    iteratees?: ISortByFunction<T> | keyof T | (ISortByFunction<T> | keyof T)[] | ISortBy<T>[] | undefined,
+): T[] => baseSortBy([...values], iteratees);
